d24c35825c78636635566e4abe4395907d217c0d
mkoohafkan <michael.koohafkan@gmail.com>
2019-04-14 13:05:02 -0700

    object dropping functions

---
+++ R/drop.r
@@ -0,0 +1,50 @@
+#' Drop HDF groups, datasets, and attributes
+#'
+#' Generic helper for dropping HDF objects.
+#'
+#' @param what The type of object to drop.
+#' @param path The target location of the object.
+#'
+#' @keywords internal
+drop = function(what = c("FILE", "GROUP", "DATASET", "ATTRIBUTE"),
+	path) {
+	script = sprintf('DROP %s "%s"', what, path)
+	res = execute_with_memory(script, stop.on.error = FALSE)
+	if (!is.null(res)) {
+		error.type = get_key(res, hql_error_types(), TRUE)
+		if (error.type != "HDFQL_ERROR_NOT_FOUND") {
+			stop(HDFql.constants$hdfql_error_get_message())
+		}
+	}
+	invisible(NULL)
+}
+
+#' @describeIn drop Drop HDF dataset.
+#'
+#' @param dataset The dataset to drop.
+#' @inheritParams drop
+#'
+#' @keywords internal
+drop_dataset = function(dataset) {
+	drop("DATASET", dataset)
+}
+
+#' @describeIn drop Drop HDF dataset.
+#'
+#' @param group The group to drop.
+#' @inheritParams drop
+#'
+#' @keywords internal
+drop_group = function(group) {
+	drop("GROUP", group)
+}
+
+#' @describeIn drop Drop HDF attribute.
+#'
+#' @param attribute The attribute to drop.
+#' @inheritParams drop
+#'
+#' @keywords internal
+drop_attribute = function(attribute) {
+	drop("ATTRIBUTE", attribute)
+}
