bbe34b9bdbeec32ac2a6fdd9a14524df26dca7d5
mkoohafkan <michael.koohafkan@gmail.com>
2019-04-17 08:51:26 -0700

    export drop functions

--- R/drop.r
+++ R/drop.r
@@ -1,9 +1,8 @@
 #' Drop HDF groups, datasets, and attributes
 #'
-#' Generic helper for dropping HDF objects.
-#'
-#' @param what The type of object to drop.
-#' @param path The target location of the object.
+#' Drop a datset, attribute, or group from 
+
+#' Drop HDF objects.
 #'
 #' @keywords internal
 drop = function(what = c("FILE", "GROUP", "DATASET", "ATTRIBUTE"),
@@ -21,30 +20,47 @@ drop = function(what = c("FILE", "GROUP", "DATASET", "ATTRIBUTE"),
 
 #' @describeIn drop Drop HDF dataset.
 #'
+#' @param file The HDF file to search.
 #' @param dataset The dataset to drop.
-#' @inheritParams drop
-#'
-#' @keywords internal
-drop_dataset = function(dataset) {
+#' @param recursive If `TRUE`, drop all child groups and datasets.
+#' @export
+hql_drop_dataset = function(file, dataset, recursive = FALSE) {
+  stop_not_loaded()
+  use_file(file)
+  on.exit(close_file(file))
+  if (!recursive) {
+    sub.groups = list("GROUP", path)
+    sub.datasets = list("DATASET", path)
+    if (length(c(sub.groups, sub.datasets)) > 0L) {
+      stop(path, 'contains groups or datasets but ',
+      'argument "recursive" is FALSE.')
+    }
+  }
 	drop("DATASET", dataset)
 }
 
-#' @describeIn drop Drop HDF dataset.
+#' @describeIn drop Drop HDF group.
 #'
 #' @param group The group to drop.
-#' @inheritParams drop
+#' @inheritParams hql_drop_dataset
 #'
-#' @keywords internal
-drop_group = function(group) {
+#' @export
+hql_drop_group = function(file, group) {
+  stop_not_loaded()
+  use_file(file)
+  on.exit(close_file(file))
 	drop("GROUP", group)
 }
 
 #' @describeIn drop Drop HDF attribute.
 #'
 #' @param attribute The attribute to drop.
-#' @inheritParams drop
+#' @inheritParams hql_drop_dataset
 #'
-#' @keywords internal
-drop_attribute = function(attribute) {
+#' @export
+hql_drop_attribute = function(file, attribute) {
+  stop_not_loaded()
+  use_file(file)
+  on.exit(close_file(file))
 	drop("ATTRIBUTE", attribute)
 }
