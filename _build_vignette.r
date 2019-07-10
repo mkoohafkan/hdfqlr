91d441d671b02934e83090a6c1580e7f83254334
mkoohafkan <michael.koohafkan@gmail.com>
2019-07-10 11:16:31 -0700

    vignette updates

--- _build_vignette.r
+++ _build_vignette.r
@@ -1,6 +1,4 @@
 devtools::load_all()
 knitr::knit("vignettes/_quickstart.rmd", "vignettes/quickstart.rmd")
-file.copy("vignettes/quickstart.rmd", "vignettes/quickstart.md",
-  overwrite = TRUE)
-
 knitr::knit("vignettes/_benchmark.rmd", "vignettes/benchmark.rmd")
+knitr::knit("vignettes/_lowlevel.rmd", "vignettes/lowlevel.rmd")
