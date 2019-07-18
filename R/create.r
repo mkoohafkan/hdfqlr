069ddc6a12c64e16718ef137872c521aa66bd519
mkoohafkan <michael.koohafkan@gmail.com>
2019-07-18 08:31:13 -0700

    now exporting function for creating groups
    also improved documentation

--- R/create.r
+++ R/create.r
@@ -1,5 +1,22 @@
 #' Create HDF Files, Datasets or Attribute
+#'
+#' Create HDF files and groups.
+#'
+#' @name hql_create
+#'
+#' @examples
+#' if(hql_is_loaded()){
+#'   tf = tempfile(fileext = ".h5")
+#'   hql_create_file(tf)
+#'   hql_create_group("group1")
+#'   hql_close_file(tf)
+#' }
+#'
+#' @name hql_write
+NULL
 
+
+#' Create HDF Object
 #'
 #' Generic helper for creating HDF objects.
 #'
@@ -13,60 +30,63 @@
 #'   attribute, or dataset.
 #' @param parallel If `TRUE`, use parallel capabilities.
 #'
-
-#' Create HDF Object
-#'
-#' Generic helper for creating HDF objects.
-#'
 #' @keywords internal
 create = function(what = c("FILE", "GROUP", "DATASET", "ATTRIBUTE"),
-	path, data.type, size, overwrite = FALSE, parallel = FALSE) {
-	what = match.arg(toupper(what), c("FILE", "GROUP", "DATASET",
-		"ATTRIBUTE"))
-	pre = ""
-	if (overwrite) {
-		pre = paste(pre, "TRUNCATE")
-	}
-	if (parallel && what != "GROUP") {
-		pre = paste(pre, "PARALLEL")
-	}
-	post = ""
-	if (what %in% c("DATASET", "ATTRIBUTE")) {
-		if (!is.null(size)) {
-			dsize = sprintf("(%s)", paste(size, collapse = ", "))
-		} else {
-			dsize = ""
-		}
-		if (!(data.type %in% gsub("^HDFQL_", "", names(hql_data_types())))) {
-			stop('Object data type "', data.type, '" not recognized')
-		}
-		post = sprintf("AS %s %s", data.type, dsize)
-	}
-	script = sprintf('CREATE %s %s "%s" %s', pre, what, path, post)
-	execute_with_memory(script)
+  path, data.type, size, overwrite = FALSE, parallel = FALSE) {
+  what = match.arg(toupper(what), c("FILE", "GROUP", "DATASET",
+    "ATTRIBUTE"))
+  pre = ""
+  if (overwrite) {
+    pre = paste(pre, "TRUNCATE")
+  }
+  if (parallel && what != "GROUP") {
+    pre = paste(pre, "PARALLEL")
+  }
+  post = ""
+  if (what %in% c("DATASET", "ATTRIBUTE")) {
+    if (!is.null(size)) {
+      dsize = sprintf("(%s)", paste(size, collapse = ", "))
+    } else {
+      dsize = ""
+    }
+    if (!(data.type %in% gsub("^HDFQL_", "", names(hql_data_types())))) {
+      stop('Object data type "', data.type, '" not recognized')
+    }
+    post = sprintf("AS %s %s", data.type, dsize)
+  }
+  script = sprintf('CREATE %s %s "%s" %s', pre, what, path, post)
+  execute_with_memory(script)
 }
 
-#' @describeIn create Create HDF file.
+#' @describeIn hql_create Create HDF file.
 #'
 #' @param file The HDF file to create.
 #' @inheritParams create
 #'
 #' @export
 hql_create_file = function(file, overwrite = FALSE, parallel = FALSE) {
-	if (!dir.exists(dirname(file))) {
-		dir.create(dirname(file), recursive = TRUE)
-	}
-	create("FILE", file, overwrite = overwrite, parallel = parallel)
+  if (!dir.exists(dirname(file))) {
+    dir.create(dirname(file), recursive = TRUE)
+  }
+  create("FILE", file, overwrite = overwrite, parallel = parallel)
+}
+
+#' @describeIn hql_create Create HDF group.
+#' @param group The group to create.
+#' @inheritParams create
+#' 
+#' @export
+hql_create_group = function(group, overwrite = FALSE) {
+  create_group(group, overwrite)
 }
 
 #' @describeIn create Create HDF group.
 #'
-#' @param group The group to create.
-#' @inheritParams create
+#'@inheritParams hql_create_group
 #'
 #' @keywords internal
 create_group = function(group, overwrite = FALSE) {
-	create("GROUP", group, overwrite = overwrite)  
+  create("GROUP", group, overwrite = overwrite)
 }
 
 #' @describeIn create Create HDF dataset.
@@ -76,8 +96,8 @@ create_group = function(group, overwrite = FALSE) {
 #'
 #' @keywords internal
 create_dataset = function(dataset, data.type, size = NULL,
-	overwrite = FALSE, parallel = FALSE) {
-	create("DATASET", dataset, data.type, size, overwrite, parallel)
+  overwrite = FALSE, parallel = FALSE) {
+  create("DATASET", dataset, data.type, size, overwrite, parallel)
 }
 
 #' @describeIn create Create HDF attribute.
@@ -87,6 +107,6 @@ create_dataset = function(dataset, data.type, size = NULL,
 #'
 #' @keywords internal
 create_attribute = function(attribute, data.type, size = NULL,
-	overwrite = FALSE, parallel = FALSE) {
-	create("ATTRIBUTE", attribute, data.type, size, overwrite, parallel)
+  overwrite = FALSE, parallel = FALSE) {
+  create("ATTRIBUTE", attribute, data.type, size, overwrite, parallel)
 }
