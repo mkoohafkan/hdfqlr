ee6dddef782aefd945264ed28f2f4a064f62debc
mkoohafkan <michael.koohafkan@gmail.com>
2019-07-19 08:14:54 -0700

    better warning message when package fails to load on start

--- R/zzz.r
+++ R/zzz.r
@@ -7,7 +7,7 @@
 		tryCatch(hql_load(hql.paths$install),
 			error = function(e)
 			  warning('Could not automatically connect to HDFql library in "',
-				hql.paths$install, '".', call. = FALSE))
+				hql.paths$install, '".', "\n", e, call. = FALSE))
   } else {
     packageStartupMessage('Connect to HDFql R drivers by calling ',
       "\n\n\t", 'hql_load("path/to/HDFql-x.x.x")',
