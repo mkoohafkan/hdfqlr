300fa37943d64e79059e5da1c3db24e709ceeb7a
mkoohafkan <michael.koohafkan@gmail.com>
2019-07-22 13:00:45 -0700

    don't catch error in dyn.load

--- R/zzz.r
+++ R/zzz.r
@@ -4,11 +4,7 @@
 
 .onAttach = function(libname, pkgname) {
   if (path_from_options(TRUE)) {
-		tryCatch(hql_load(hql.paths$install),
-			error = function(e)
-			  warning('Could not automatically connect to HDFql library:\n  ',
-        paste(file.path(hql.paths$install, hql.paths$dll), collapse = "\n  "),
-        "\n", e, call. = FALSE))
+		hql_load(hql.paths$install)
   } else {
     packageStartupMessage('Connect to HDFql R drivers by calling ',
       "\n\n\t", 'hql_load("path/to/HDFql-x.x.x")',
